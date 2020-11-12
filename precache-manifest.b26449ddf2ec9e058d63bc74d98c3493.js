9e5a6a5acf9165947b14ac05c527fe56624cc4d8
Annabeth Carroll <anncarroll@indeed.com>
2020-11-11 17:37:23 -0800

    Updates

---
+++ precache-manifest.b26449ddf2ec9e058d63bc74d98c3493.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39172407a5353432ccd830f8418c91e1",
+    "url": "/boodlesite/index.html"
+  },
+  {
+    "revision": "65a30e0aa1b1f38a49cf",
+    "url": "/boodlesite/static/css/main.c6fd2a0b.chunk.css"
+  },
+  {
+    "revision": "986f4eac59dabbd575a9",
+    "url": "/boodlesite/static/js/2.fe430af4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/boodlesite/static/js/2.fe430af4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65a30e0aa1b1f38a49cf",
+    "url": "/boodlesite/static/js/main.49a68357.chunk.js"
+  },
+  {
+    "revision": "10ac2a0656e1179b54c4",
+    "url": "/boodlesite/static/js/runtime-main.b1f59aa1.js"
+  },
+  {
+    "revision": "017f18c90cad0506d7cd0da17b852857",
+    "url": "/boodlesite/static/media/Analytics.017f18c9.png"
+  },
+  {
+    "revision": "506d40181ed284155668c380009a3878",
+    "url": "/boodlesite/static/media/AnnabethCarroll.506d4018.pdf"
+  },
+  {
+    "revision": "1cfd2d61ce9faa80a2886bfe4c63c687",
+    "url": "/boodlesite/static/media/affinity-map.1cfd2d61.jpeg"
+  },
+  {
+    "revision": "35a3f20703c12087cf7978442f34bd4a",
+    "url": "/boodlesite/static/media/brand-voice.35a3f207.png"
+  },
+  {
+    "revision": "1a2b8d612852f5b949cade6f9d328909",
+    "url": "/boodlesite/static/media/colors.1a2b8d61.png"
+  },
+  {
+    "revision": "304e78940f617e2f7343a63227069b26",
+    "url": "/boodlesite/static/media/competitive-analysis.304e7894.png"
+  },
+  {
+    "revision": "7741a5499e09740e8003871b496f0c9a",
+    "url": "/boodlesite/static/media/cover.7741a549.jpg"
+  },
+  {
+    "revision": "a2e39548698260a6bfad6213f7b61abf",
+    "url": "/boodlesite/static/media/cover.a2e39548.jpg"
+  },
+  {
+    "revision": "0f5043d3ca2d9579646d12dfa8a528c1",
+    "url": "/boodlesite/static/media/emotion.0f5043d3.png"
+  },
+  {
+    "revision": "b750e7aaa029fdba22263955a8b1e76f",
+    "url": "/boodlesite/static/media/feature-prioritization.b750e7aa.png"
+  },
+  {
+    "revision": "69e45c46b36cbffb0b5fd7f52fa334a3",
+    "url": "/boodlesite/static/media/fullpage.69e45c46.png"
+  },
+  {
+    "revision": "4dd7b3ed3ab97c7ef98deeef419b311c",
+    "url": "/boodlesite/static/media/heatmap-hero.4dd7b3ed.png"
+  },
+  {
+    "revision": "225fbfbb4b44577c333f595136268469",
+    "url": "/boodlesite/static/media/heatmaps1.225fbfbb.png"
+  },
+  {
+    "revision": "e64dd9618b02f34ef075922071beb5f2",
+    "url": "/boodlesite/static/media/heatmaps2.e64dd961.png"
+  },
+  {
+    "revision": "0f3cd31b17590f864d7860cd8593313c",
+    "url": "/boodlesite/static/media/heatmaps3.0f3cd31b.png"
+  },
+  {
+    "revision": "b453d8a3fe3ba761d2fd06bbc09eb96a",
+    "url": "/boodlesite/static/media/heatmaps4.b453d8a3.png"
+  },
+  {
+    "revision": "36ac9b275d1c7871331319ec99ecd748",
+    "url": "/boodlesite/static/media/hero.36ac9b27.png"
+  },
+  {
+    "revision": "279e34c2f39200219df7ad8cc6622fdd",
+    "url": "/boodlesite/static/media/hi-fi-proto.279e34c2.png"
+  },
+  {
+    "revision": "8bbbff59fb22f109080abf4f18bb8237",
+    "url": "/boodlesite/static/media/hover-hero.8bbbff59.gif"
+  },
+  {
+    "revision": "0ea147511ffd4ff9d80cef254dc9816a",
+    "url": "/boodlesite/static/media/hover.0ea14751.gif"
+  },
+  {
+    "revision": "409de66b01661203f0569b2ff566fe68",
+    "url": "/boodlesite/static/media/hoverfree.409de66b.png"
+  },
+  {
+    "revision": "6c23bd2b9ecc1443751b225daaae9f84",
+    "url": "/boodlesite/static/media/hubspot.6c23bd2b.png"
+  },
+  {
+    "revision": "fe53aab77326fc4aacf84c52e51c6639",
+    "url": "/boodlesite/static/media/icons.fe53aab7.png"
+  },
+  {
+    "revision": "de158b331c4a6f15245ae3e2a942baf5",
+    "url": "/boodlesite/static/media/logo.de158b33.png"
+  },
+  {
+    "revision": "bdd8e9d87c99b249729732c080639867",
+    "url": "/boodlesite/static/media/me.bdd8e9d8.jpg"
+  },
+  {
+    "revision": "89032d6be97348900ce9e36b40997788",
+    "url": "/boodlesite/static/media/pega.89032d6b.png"
+  },
+  {
+    "revision": "c78f3dffb5afa57cfda756ccd4e73f0d",
+    "url": "/boodlesite/static/media/persona-img.c78f3dff.jpeg"
+  },
+  {
+    "revision": "8a9278826076df8cf7e667dc40657428",
+    "url": "/boodlesite/static/media/react.8a927882.png"
+  },
+  {
+    "revision": "c458725fd6897a2835241eaa36c9edac",
+    "url": "/boodlesite/static/media/results.c458725f.png"
+  },
+  {
+    "revision": "69e6c456fb0d5026ce7e5094c5b9fe49",
+    "url": "/boodlesite/static/media/sketch-small.69e6c456.jpeg"
+  },
+  {
+    "revision": "004af459c26af75c0f102ebabdf8a01d",
+    "url": "/boodlesite/static/media/sketch.004af459.png"
+  },
+  {
+    "revision": "8e9775407f0876a44efc8d07cea8f2e7",
+    "url": "/boodlesite/static/media/start-tour-hero.8e977540.png"
+  },
+  {
+    "revision": "46cd3810c30136a1b23080bb33269316",
+    "url": "/boodlesite/static/media/step-v1.46cd3810.png"
+  },
+  {
+    "revision": "0e56c491dbd96d1a095ba1025f2a800f",
+    "url": "/boodlesite/static/media/step-v2.0e56c491.png"
+  },
+  {
+    "revision": "a2ad739a6273696f6b320b6fd3c6f6a9",
+    "url": "/boodlesite/static/media/testimonial+dark+headshot.a2ad739a.png"
+  },
+  {
+    "revision": "3f734f6cf0409c42e9769aed9661a494",
+    "url": "/boodlesite/static/media/testimonial+dark+no+headshot.3f734f6c.png"
+  },
+  {
+    "revision": "e530dff6ba9f673e9350d65649d126d0",
+    "url": "/boodlesite/static/media/testimonial+light+no+headshot.e530dff6.png"
+  },
+  {
+    "revision": "8cd5b6330e798abeb61c13a5c3d866fb",
+    "url": "/boodlesite/static/media/tour-dashboard.8cd5b633.png"
+  },
+  {
+    "revision": "575e449e66a33dd80b341e3e3d4b7a48",
+    "url": "/boodlesite/static/media/twine.575e449e.png"
+  },
+  {
+    "revision": "a3ca5f09427db4a43a597994765e289e",
+    "url": "/boodlesite/static/media/typography.a3ca5f09.png"
+  },
+  {
+    "revision": "b0599bb279fe7da31a4587d5b50b23a9",
+    "url": "/boodlesite/static/media/user-flow.b0599bb2.jpeg"
+  },
+  {
+    "revision": "9d1b617f93a6d5121d187903a132d1a8",
+    "url": "/boodlesite/static/media/vassar.9d1b617f.png"
+  },
+  {
+    "revision": "3fc7bc4ef8b18f16b07e3fb647009116",
+    "url": "/boodlesite/static/media/walkme+editor.3fc7bc4e.png"
+  },
+  {
+    "revision": "72ff866525185c8da78ae1f5e39719cc",
+    "url": "/boodlesite/static/media/wireframe.72ff8665.png"
+  },
+  {
+    "revision": "86a9c94e98b383ec3cb3bf89b1bc5bb7",
+    "url": "/boodlesite/static/media/wireframes.86a9c94e.png"
+  }
+]);
